8469a9dd573e1c62e7aae7a42e651eaa15ef8ee5
santtuniskanen <santtuniska02@gmail.com>
2023-11-01 10:07:11 +0200

    login thing

---
+++ login-thing/app/page.js
@@ -0,0 +1,24 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Home() {
+  const [login, setLogin] = useState(false);
+  return (
+    <div>
+      <h4>Arvo on {login.toString()}</h4>
+      <Login login={login} setLogin={setLogin} />
+    </div>
+  );
+}
+
+function Login({ login, setLogin }) {
+  const buttonLabel = login ? "Logout" : "Login";
+
+  return (
+    <div>
+      {login ? <h2>Welcome!</h2> : <h4>Unauthorized!</h4>}
+      <button onClick={() => setLogin(!login)}>{buttonLabel}</button>
+    </div>
+  );
+}
